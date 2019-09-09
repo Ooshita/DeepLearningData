c0e7e1f0539dbe7b2f1d79ece98ec9c1ed3c52f3
Oshita <ponkichi0213@gmail.com>
2019-09-10 07:59:45 +0900

    plotを表示するところまで

--- DataRecognization.py
+++ DataRecognization.py
@@ -3,6 +3,7 @@ import numpy as np
 import xlrd
 import pandas as pd
 import matplotlib.pyplot as plt
+import japanize_matplotlib
 
 FILE = sys.argv[1]
 book = xlrd.open_workbook(FILE)
@@ -17,6 +18,23 @@ for i in range(s.row_len(0)):
     elif s.cell_type(1,i) == 2:
         data_type.append(int)
 
-df = pd.read_excel(sys.argv[1],dtype={0:data_type[0], 1:data_type[1],2:data_type[2]})
+df = pd.read_excel(sys.argv[1], dtype={0:data_type[0], 1:data_type[1],2:data_type[2]})
 
 print(df.dtypes)
+columns = df.columns
+for i, d in enumerate(data_type):
+    if d == str:
+        plt.subplot(2,2,1+i)
+        categories = np.unique(df[columns[i]])
+        c_dict = {}
+        for c in categories:
+            c_dict[c] = (df[columns[i]] == c).sum()
+        print(c_dict)
+        plt.bar(list(c_dict.keys()), list(c_dict.values()))
+        plt.title(columns[i])
+    if d == int:
+        plt.subplot(2,2,1+i)
+        plt.plot(df[columns[i]])
+        plt.title(columns[i])
+
+plt.show()
